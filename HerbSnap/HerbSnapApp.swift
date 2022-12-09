3ee76429dc5d6dc749c9ce4a13cf2767101353a6
Jonathan Diebel <jonathan.diebel@web.de>
2022-12-09 21:33:43 +0100

    Initial Commit

---
+++ HerbSnap/HerbSnapApp.swift
@@ -0,0 +1,17 @@
+//
+//  HerbSnapApp.swift
+//  HerbSnap
+//
+//  Created by Jonathan Diebel on 09.12.22.
+//
+
+import SwiftUI
+
+@main
+struct HerbSnapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
