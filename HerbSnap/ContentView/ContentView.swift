af78c2df070fcfc6a33ebbc7d29b93bb05e7ba52
Jonathan Diebel <jonathan.diebel@web.de>
2022-12-27 11:54:24 +0100

    started working on live camera feed

--- HerbSnap/ContentView/ContentView.swift
+++ HerbSnap/ContentView/ContentView.swift
@@ -11,13 +11,42 @@ struct ContentView: View {
     @State private var capturedImage: UIImage? = nil
     @State private var isCustomCameraViewPresented = false
     
+    let cameraService = CameraService()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, HerbSnap")
+        ZStack {
+            CameraView(cameraService: cameraService) { result in
+                switch result {
+                case .success(let photo):
+                    if let data = photo.fileDataRepresentation() {
+                        capturedImage = UIImage(data: data)
+                    } else {
+                        print("Error: No image data found.")
+                    }
+                case .failure(let err):
+                    print(err.localizedDescription)
+                }
+            }
+            
+            VStack {
+                Spacer()
+                // Button for capturing the photo
+                Button(action: {
+                    cameraService.capturePhoto()
+                }, label: {
+                    Image(systemName: "circle")
+                        .font(.system(size: 72))
+                        .foregroundColor(.white)
+                })
+                .padding(.bottom)
+            }
         }
-        .padding()
+//        VStack {
+//            Image(systemName: "globe")
+//                .imageScale(.large)
+//                .foregroundColor(.accentColor)
+//            Text("Hello, HerbSnap")
+//        }
+//        .padding()
     }
 }
