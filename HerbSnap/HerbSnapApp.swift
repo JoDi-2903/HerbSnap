b6b18050e45a8d1983280fd5232673192ad91347
Jonathan Diebel <jonathan.diebel@web.de>
2022-12-30 13:50:05 +0100

    deactivated coloured topSafeArea when no image is shown

--- HerbSnap/HerbSnapApp.swift
+++ HerbSnap/HerbSnapApp.swift
@@ -28,11 +28,13 @@ struct HerbSnapApp: App {
                 }
                 
                 // Transparent color for the top safeArea
-                GeometryReader { reader in
-                    Color(red: 0.1, green: 0.1, blue: 0.1)
-                        .frame(height: reader.safeAreaInsets.top, alignment: .top)
-                        .opacity(0.6)
-                        .ignoresSafeArea(edges: .top)
+                if activeView == 1 || activeView == 2 {
+                    GeometryReader { reader in
+                        Color(red: 0.1, green: 0.1, blue: 0.1)
+                            .frame(height: reader.safeAreaInsets.top, alignment: .top)
+                            .opacity(0.6)
+                            .ignoresSafeArea(edges: .top)
+                    }
                 }
             }
         }
