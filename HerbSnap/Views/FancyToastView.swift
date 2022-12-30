19a728564abfc223209906b9ac04244e77268bb2
Jonathan Diebel <jonathan.diebel@web.de>
2022-12-30 11:36:28 +0100

    added FancyToastView

---
+++ HerbSnap/Views/FancyToastView.swift
@@ -0,0 +1,63 @@
+//
+//  FancyToastView.swift
+//  HerbSnap
+//
+//  Created by Jonathan Diebel on 30.12.22.
+//  Credits to Farhan Adji ("Create a Fancy Toast Component Using SwiftUI")
+//  Source: https://betterprogramming.pub/swiftui-create-a-fancy-toast-component-in-10-minutes-e6bae6021984
+//
+
+import SwiftUI
+
+struct FancyToastView: View {
+    @State var herbName: String = "Basilikum"
+    //    @Binding var herbName: String
+    // Binomial name
+    
+    
+    var body: some View {
+        Button(action: {print("Herb label tapped.")}) {
+            VStack(alignment: .leading) {
+                HStack(alignment: .top) {
+                    Image("Basil")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 120)
+                        .cornerRadius(8)
+                    
+                    VStack(alignment: .leading) {
+                        
+                        Text(herbName)
+                            .font(.system(size: 18, weight: .semibold))
+                            .foregroundColor(.black)
+                        
+                        Text("Ocimum basilicum")
+                            .font(.system(size: 13))
+                            .foregroundColor(Color.black.opacity(0.6))
+                    }
+                    .padding(.top)
+                    
+                    Spacer()
+                }
+            }
+            .background(Color.white)
+            .overlay(
+                Rectangle()
+                    .fill(Color.accentColor)
+                    .frame(width: 6)
+                    .clipped()
+                , alignment: .leading
+            )
+            .frame(minWidth: 0, maxWidth: .infinity)
+            .cornerRadius(8)
+            .shadow(color: Color.black.opacity(0.25), radius: 4, x: 0, y: 1)
+            .padding(.horizontal, 17)
+        }
+    }
+}
+
+struct FancyToastView_Previews: PreviewProvider {
+    static var previews: some View {
+        FancyToastView()
+    }
+}
