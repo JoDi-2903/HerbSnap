0a27dd82f593390b0030d8b8d10084625586549f
Jonathan Diebel <jonathan.diebel@web.de>
2022-12-30 22:20:41 +0100

    translate output of classifier to input values of toast

--- HerbSnap/Views/ResultsView.swift
+++ HerbSnap/Views/ResultsView.swift
@@ -60,11 +60,32 @@ struct ResultsView: View {
             let classificationResult = performImageClassification(img: capturedImage!)
             self.classificationLabel = classificationResult.0
             self.classificationLabelProps = classificationResult.1
-            let classLabelPropsSorted = self.classificationLabelProps.sorted { return $0.value > $1.value }
             
-            print(classificationLabel)
+            let classLabelPropsSorted = self.classificationLabelProps.sorted { return $0.value > $1.value }
             print(classLabelPropsSorted)
-            toast = FancyToast(herbName: [classificationLabel, "Toast2"], binomialName: ["Lorem ipsum", "Lala"], herbImageName: ["Basil", "Basil"], doubleToast: true)
+            print(classLabelPropsSorted[0])
+            print(classLabelPropsSorted[1].key)
+            
+            var herbName: [String] = []
+            var binomialName: [String] = []
+            var herbImageName: [String] = []
+            var doubleToast: Bool = false
+
+            // Use the probabilities to check whether the result is clear or not
+            if (classLabelPropsSorted[0].value - classLabelPropsSorted[1].value) <= 0.2 {
+                herbName.append(contentsOf: [classLabelPropsSorted[0].key, classLabelPropsSorted[1].key])
+                binomialName.append(contentsOf: [binomialHerbName[classLabelPropsSorted[0].key] ?? "n/a", binomialHerbName[classLabelPropsSorted[1].key] ?? "n/a"])
+                herbImageName.append(contentsOf: [classLabelPropsSorted[0].key, classLabelPropsSorted[1].key])
+                doubleToast = true
+            } else {
+                herbName.append(classLabelPropsSorted[0].key)
+                binomialName.append(binomialHerbName[classLabelPropsSorted[0].key] ?? "n/a")
+                herbImageName.append(classLabelPropsSorted[0].key)
+                doubleToast = false
+            }
+
+            // Show FancyToast with result(s) after all parameters are set
+            toast = FancyToast(herbName: herbName, binomialName: binomialName, herbImageName: herbImageName, doubleToast: doubleToast)
         }
     }
     
